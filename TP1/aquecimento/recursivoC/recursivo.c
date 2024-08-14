79929a66906630097b41ad58f4493b924c1ff1a5
ygabsxw <gabrieldrvianna@gmail.com>
2024-08-14 14:45:23 -0300

    novas atividades

--- TP1/aquecimento/recursivoC/recursivo.c
+++ TP1/aquecimento/recursivoC/recursivo.c
@@ -3,17 +3,31 @@
 #include <string.h>
 #include <stdlib.h>
 
-int maiusculas (char *palavra, int i) {
-    int count = 0;
-    int length = srtlen(palavra);
+int maiusculas (char *palavra, int i, int count) {
+    int length = strlen(palavra);
 
-    if(palavra[i] >= 'A' && palavra [i] <= 'Z') {
-        count+1;
-        maiusculas(palavra, i+1);
+    if (i >= length) {
+        return count;
+    } // retorna o contagem de maiusculas tirando ela da recursividade
+
+    if (palavra[i] >= 'A' && palavra[i] <= 'Z') {
+        count++;  
     }
+
+    return maiusculas(palavra, i + 1, count);
+
 }
 
 int main () {
+    char palavra[5000];
+    int i = 0;
+    int c = 0;
+    
+    scanf(" %[^\n\r]", palavra);
+    while(strcmp(palavra, "FIM")) {
+        printf("%d\n", maiusculas(palavra, i, c));
+        scanf(" %[^\n\r]", palavra);
+    }
 
     return 0;
 }
--- TP1/aquecimento/recursivoJava/recursivo.java
+++ TP1/aquecimento/recursivoJava/recursivo.java
@@ -1,5 +1,30 @@
-package aquecimento.recursivoJava;
+import java.util.Scanner;
 
 public class recursivo {
-    
+    public static int maiusculas(String palavra, int i, int count) {
+        if(i >= palavra.length()) {
+            return count;
+        }
+
+        if(palavra.charAt(i) >= 'A' && palavra.charAt(i) <= 'Z') {
+            count++;
+        }
+
+        return maiusculas(palavra, i + 1, count);
+    }
+
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String palavra = sc.nextLine();
+        int i = 0;
+        int c = 0;
+
+        while (!(palavra.length() == 3 && palavra.charAt(0) == 'F' && palavra.charAt(1) == 'I' && palavra.charAt(2) == 'M')) {
+            System.out.println(maiusculas(palavra, i, c));
+
+            palavra = sc.nextLine();
+        }
+        sc.close();
+    }
+
 }
