7e40ba48c6bce3850ecea61548c03619ecdfd659
ygabsxw <gabrieldrvianna@gmail.com>
2024-08-13 22:02:28 -0300

    palindromo java

--- TP1/ex1/palindromo.java
+++ TP1/ex1/palindromo.java
@@ -1,7 +1,26 @@
 import java.util.Scanner;
 
-public class main {
-    public static void main (String[], args) {
+public class palindromo {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner (System.in);
+        String palavra = sc.nextLine();
         
+        while (!palavra.equals("FIM")) {
+            boolean ehPalindromo = true;
+            for (int i = 0 ; i < palavra.length() / 2 ; i++) {
+                if (palavra.charAt(i) != palavra.charAt(palavra.length() - 1 - i)) {
+                    ehPalindromo = false;
+                    i = palavra.length();
+                }
+            }
+            if (ehPalindromo) {
+                System.out.println("SIM");
+            } else {
+                System.out.println("NAO");
+            }
+            palavra = sc.nextLine();
+        }
+        sc.close();
     }
+    
 }
