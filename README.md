390373fc64b60d508cdb7360f17e345df596bc24
ygabsxw <gabrieldrvianna@gmail.com>
2024-08-26 15:36:41 -0300

    exercicio lab03

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # ProjetoAEDS
+
+Atividades em Java e C do Trabalho Prático de AEDS 2 da Faculdade PUCMinas no curso Ciências da Computação
+
+Atividades em Java e C do BeeCrowd para estudos de AEDS 2
---
+++ TP1/aquecimento/lab03/BalancoParenteses.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class BalancoParenteses {    
+    private static boolean contaParenteses(String expressao) {
+        int tam = expressao.length();
+        int qtdParentese = 0;
+        boolean parentesesCorretos = false;
+
+        for(int i = 0 ; i < tam ; i++) {
+            if(expressao.charAt(i) == '(') {
+                qtdParentese++;
+            } else if(expressao.charAt(i) == ')') {
+                qtdParentese--;
+            }
+        
+            if(qtdParentese < 0) {
+                parentesesCorretos = false;
+                i = tam;
+            }
+        }
+
+        if(qtdParentese == 0) {
+            parentesesCorretos = true;
+        }
+
+        return parentesesCorretos;
+    }
+
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String expressao = sc.nextLine();
+
+        while(!(expressao.length() == 3 && expressao.charAt(0) == 'F' && expressao.charAt(1) == 'I' && expressao.charAt(2) == 'M')) {
+            if(contaParenteses(expressao)) {
+                System.out.println("correto");
+            } else {
+                System.out.println("incorreto");
+            }
+
+            expressao = sc.nextLine();
+        }
+    }
+}
